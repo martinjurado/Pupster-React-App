1de5df8d67ad50495c7994d48f4cf31ffc2643a3
unknown <juradomartinbenedict@gmail.com>
2019-04-12 13:37:10 -0400

    added search component, fixed margins, modified header picture, added states

---
+++ src/components/search/search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css"
+
+function SearchBar(props) {
+  return (
+    <form>
+      <div className="form-group">
+        <br />
+        <input
+          placeholder="Search for a pooch"
+          value={props.value}
+          onChange={props.handleInputChange}
+          className="form-control"
+          name="search"
+          type="text"
+        />
+        <br />
+        <button onClick={props.handleSubmit} className="btn btn-info">Bark</button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchBar;
